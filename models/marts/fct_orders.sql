8e5a6db6506cb7d862fb44be9e907c44919af9a6
Javier Fernández Morilla <javierfernandezmorilla@gmail.com>
2024-08-30 07:42:43 +0000

    defer test DEV

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -5,6 +5,7 @@ select
     p.fecha_pedido,
     'defer_test' as defer_test,
     'defer_test2' as defer_test2,
+    'defer_test3' as defer_test3
 
 from {{ ref('usuarios') }} u inner join {{ ref('pedidos') }} p
     on u.usuario_id = p.usuario_id
