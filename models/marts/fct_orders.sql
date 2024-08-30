7f5e42416e021f58b3ff0b1586b7fdafab51c0b0
Javier Fernández Morilla <javierfernandezmorilla@gmail.com>
2024-08-30 10:02:29 +0000

    ci_test7

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -11,7 +11,8 @@ select
     'ci_test3' as ci_test3,
     'ci_test4' as ci_test4,
     'ci_test5' as ci_test5,
-    'ci_test6' as ci_test6     
+    'ci_test6' as ci_test6,
+    'ci_test7' as ci_test7         
 
 from {{ ref('usuarios') }} u inner join {{ ref('pedidos') }} p
     on u.usuario_id = p.usuario_id
